d85dbfb40ee70ea65d0c84fd4a888549082036c9
GabrielCarmo-S <perien.op@gmail.com>
2021-03-26 17:26:17 -0300

    Updates

---
+++ src/screens/Save/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+image: {
+  flex: 1,
+  height: '100%',
+  width: '100%',
+  justifyContent: 'flex-end'
+},
+
+save: {
+  margin: 10,
+  height: 50,
+  width: 50,
+  alignSelf: 'center'
+}
+
+})
+
+export default styles;
